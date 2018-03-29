008e1f0efddf962dd3c048b0c81304bf9e2030bb
Danny Wolf <wolf@students.olin.edu>
2018-03-29 01:03:41 -0400

    add gpio code and change mind about library

--- src/main.py
+++ src/main.py
@@ -1,9 +1,18 @@
 from flask import Flask, request
-import pigpio
+import RPi.GPIO as GPIO
 import atexit
 
+ACTUATOR_CHANNEL = 12
+
 app = Flask(__name__)
-gpio = pigpio.pi()
+
+pwm = {
+    ACTUATOR_CHANNEL: None
+}
+
+def setup_gpio():
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(ACTUATOR_CHANNEL, GPIO.OUT)
 
 @app.route('/')
 def index():
@@ -15,20 +24,29 @@ def notify():
     try:
         duration = float(str_duration)
     except ValueError:
-        content = {'error': 'duration is not a number'}
-        return content, 400
-    # gpio here
+        return 'duration is not a number', 400
+    # start pwming and store global
+    if pwm[ACTUATOR_CHANNEL] is not None:
+        return 'already doing something', 400
+    p = GPIO.PWM(ACTUATOR_CHANNEL, 0.5)
+    p.start(1)
+    pwm[ACTUATOR_CHANNEL] = p
 
-    return "honk"
+    return 'honk'
 
 @app.route('/stop')
 def stop():
-    return "stopped"
-    # gpio here
+    return 'stopped'
+    # stop doing pwm and clear reference
+    if pwm[ACTUATOR_CHANNEL] is None:
+        return 'not doing anything', 400
+    pwm[ACTUATOR_CHANNEL].stop()
+    pwm[ACTUATOR_CHANNEL] = None
+    
 
-def close_gpio():
-    gpio.stop()
+def cleanup_gpio():
+    GPIO.cleanup()
 
 if __name__ == '__main__':
-    atexit.register(close_gpio)
+    atexit.register(cleanup_gpio)
     app.run(host='0.0.0.0', port=80)
